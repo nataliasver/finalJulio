549182f960c98bef4d7986554e52855ce9d913d3
nataliasver <brujinat@gmail.com>
2014-09-17 20:54:32 -0300

    Ser carga proyecto con includes, sin resolución

---
+++ finalJulio/finalJulio.cpp
@@ -0,0 +1,20 @@
+/*
+* Autor de la solución: Natalia S. V.
+*
+*Tema: Resolución final 18-7-2014
+*
+*
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
+
