789d979caea1e2f78bf32b7910e7073dadf43ae5
nataliasver <brujinat@gmail.com>
2014-09-17 22:58:52 -0300

    Se agrega la funcion MostrarRuta

--- finalJulio/finalJulio.cpp
+++ finalJulio/finalJulio.cpp
@@ -20,7 +20,9 @@ struct ruta
 	unsigned ruta;
 } ;
 
-void inicializarM(array < array < unsigned, 100 >, 100 >);
+void InicializarM(array < array < unsigned, 100 >, 100 >);
+
+void MostrarRuta(unsigned, unsigned, array < array < unsigned, 100 >, 100 >);
 
 int main()
 {
@@ -30,3 +32,23 @@ int main()
 
 }
 
+void MostrarRuta(unsigned loc1, unsigned loc2, array < array < unsigned, 100 >, 100 > rutdis)
+ {
+	if (loc1 == loc2)
+	{
+		cout << "Las localidades ingresadas son las mismas";
+		return;
+	}
+	if (rutdis.at(loc2).at(loc1) == 0)
+	{
+		cout << "No se encuentra una ruta directa entre ambas localidades";
+	}
+	else
+	{
+		cout << "La ruta tiene un valor de: " << rutdis.at(loc2).at(loc1) << endl;
+		cout << "La distancia entre ambas localidades es de: " << rutdis.at(loc1).at(loc2) << endl;
+	}
+	return;
+
+
+ }
